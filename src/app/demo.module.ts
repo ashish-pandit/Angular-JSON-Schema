2d83ebdc08ffd9fb5d48c9258247917b7049ea9e
ashish sharma <man.codewiseinfotech@gmail.com>
2019-09-21 14:41:27 +0530

    Changes in mudule

--- src/app/demo.module.ts
+++ src/app/demo.module.ts
@@ -4,13 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatSelectModule } from '@angular/material/select';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 
 import { DemoComponent } from './demo.component';
@@ -18,21 +12,17 @@ import { DemoRootComponent } from './demo-root.component';
 
 import { routes } from './demo.routes';
 import {
-  MaterialDesignFrameworkModule, Bootstrap4FrameworkModule,
-  Bootstrap3FrameworkModule, NoFrameworkModule
+  MaterialDesignFrameworkModule  
 } from 'angular6-json-schema-form';
 
 @NgModule({
   declarations: [ DemoComponent, DemoRootComponent],
   imports: [
     BrowserModule, BrowserAnimationsModule, FlexLayoutModule, FormsModule,
-    HttpClientModule, MatButtonModule, MatCardModule, MatCheckboxModule,
-    MatIconModule, MatMenuModule, MatSelectModule, MatToolbarModule,
+    HttpClientModule, MatCardModule,
     RouterModule.forRoot(routes),
     MaterialDesignFrameworkModule,
-    Bootstrap4FrameworkModule,
-    Bootstrap3FrameworkModule,
-    NoFrameworkModule
+    
   ],
   bootstrap: [DemoRootComponent]
 })
