2ea0ee6e16aa115d72bdd927a3f3a78b52f8d6ce
ashish sharma <man.codewiseinfotech@gmail.com>
2019-09-21 13:28:41 +0530

    json schema

---
+++ README.md
@@ -0,0 +1,18 @@
+
+## Installation
+
+### To install from GitHub
+
+To install [the library and the example playground from GitHub](https://github.com/ashish-pandit/Angular-JSON-Schema.git), clone `https://github.com/ashish-pandit/Angular-JSON-Schema.git` with your favorite git program. 
+
+```shell
+git clone https://github.com/ashish-pandit/Angular-JSON-Schema.git angular6-json-schema-form
+yarn install
+yarn start
+```
+
+This should start a server with the example playground, which you can view in your browser at `http://localhost:4200`
+
+
+## Author
+Ashish Upadhyay
---
+++ src/app/demo.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'demo',
+  templateUrl: 'demo.component.html',
+})
+export class DemoComponent implements OnInit {
+  selectedExample = 'ng-jsf-flex-layout';
+  selectedExampleName = 'Flexbox layout';
+  selectedFramework = 'material-design';
+  visible = {
+    options: true,
+    schema: true,
+    form: true,
+    output: true
+  };
+  
+  formActive = false;
+  jsonFormSchema: string;
+  jsonFormStatusMessage = 'Loading form...';
+  jsonFormObject: any;
+  submittedFormData: any = null;
+  constructor( private http: HttpClient ) { }
+
+  ngOnInit() {
+    this.loadSelectedExample();
+  }
+
+  onSubmit(data: any) {
+    this.submittedFormData = data;
+    console.log('hello',this.submittedFormData)
+  }
+
+  get prettySubmittedFormData() {
+    return JSON.stringify(this.submittedFormData, null, 2);
+  }
+
+  loadSelectedExample() {
+    const exampleURL = `assets/example-schemas/${this.selectedExample}.json`;
+    this.http
+      .get(exampleURL, { responseType: 'text' })
+      .subscribe(schema => {
+        this.jsonFormSchema = schema;
+        this.generateForm(this.jsonFormSchema);
+      });
+  }
+  
+  generateForm(newFormString: string) {
+    if (!newFormString) { return; }
+    this.formActive = false;
+    try {
+      this.jsonFormObject = JSON.parse(newFormString);
+    } catch (jsonError) {
+      try {
+        const newFormObject: any = null;
+        eval('newFormObject = ' + newFormString);
+        this.jsonFormObject = newFormObject;
+      } catch (javascriptError) {
+        this.jsonFormStatusMessage =  jsonError;
+        return;
+      }
+    }
+    this.formActive = true;
+  }
+}
